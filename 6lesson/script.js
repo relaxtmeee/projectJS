5e7e5e33a7faa07bc95e920b6879b79895dacd80
Serzh <ivanovserzh99@mail.ru>
2020-10-16 22:01:40 +0300

    first commit

---
+++ 6lesson/script.js
@@ -0,0 +1,40 @@
+// ТИПЫ ДАННЫХ
+
+let number = 4;
+
+console.log(-4/0);
+console.log('string' * 9); // NaN
+
+const person = 'Alex';
+console.log(person);
+
+const bool = false;
+console.log(bool);
+
+console.log(something);
+
+let und;
+console.log(und);
+
+//есть символы еще
+
+// и bigint
+
+const obj = {
+    name: 'Serzh',
+    age: 25,
+    isMarried: false
+}
+
+console.log(obj.name);
+console.log(obj['name']);
+
+let arr = [
+    'plum.png',
+    'orange.jpg',
+    'apple.bmp',
+    6,
+    {},
+    []
+]
+console.log(arr[1]);
