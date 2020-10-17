c077f2138aaf35c2b9a60330f0eb924ec873047f
Serzh <ivanovserzh99@mail.ru>
2020-10-17 12:19:12 +0300

    add variable and obj

---
+++ script.js
@@ -0,0 +1,9 @@
+let numberOfFilms = prompt("Сколько фильмов вы просмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
