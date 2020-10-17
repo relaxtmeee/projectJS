34423c433e3ea78ebb15dccc18f21096b4d9c7f3
Serzh <ivanovserzh99@mail.ru>
2020-10-17 12:35:24 +0300

    create 4consts and log its

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt("Сколько фильмов вы просмотрели?");
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -7,3 +7,14 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
+
+const lastFilm = prompt('Один из последних просмотренных вами фильмов');
+const lastFilmScore = +prompt('Ваша оценка фильма');
+const lastFilm2 = prompt('Один из последних просмотренных вами фильмов');
+const lastFilmScore2 = +prompt('Ваша оценка фильма');
+
+personalMovieDB.movies.lastFilm = lastFilmScore;
+personalMovieDB.movies.lastFilm2 = lastFilmScore2;
+
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
